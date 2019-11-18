22b03577866d99a9b792b8b238e36e7e27a625f3
AstroKD <kdreyerinseattle@gmail.com>
2019-11-18 13:59:30 -0800

    line

--- schema.sql
+++ schema.sql
@@ -13,5 +13,11 @@ CREATE TABLE movies (
   vote_average VARCHAR(255)
 );
 
--- INSERT INTO movies (title, overview, thumbnail, release_date, vote_average);
+INSERT INTO movies (title, overview, thumbnail, release_date, vote_average) VALUES (
+  'My Awesome Title',
+  'My awesome overview of this Awesome Movie',
+  'Awesome_thumbnail_of_the_Awesome_Movie.png',
+  '2099-12-31',
+  '0'
+);
  
