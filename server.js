5462a786dc3385f5319d094d74e3e5a755df83f2
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-11-18 11:52:21 -0800

    head header footer error and index modified x server modified

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
+client.connect();
 client.on('error', err => console.error(err));
 
+//////////////////////////////////////////////////////////////////////////////////////////////////Routes/////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/', findMovies); //find a movie to watch
 
+
+function findMovies(req, res) {
+  let SQL = 'SELECT * FROM movies;';
+  return client.query(SQL)
+    .then(results => res.render('pages/index', {
+      results: results.rows
+    }))
+    .catch(() => {
+      res.render('pages/error');
+    })
+}
 
 app.listen(PORT, () => console.log(`LiStEnInG oN pOrT ${PORT}`));
