8abf911948256c07d44b85d4bbf378b848fd1825
RAN VAKNIN <ranvaknin0@gmail.com>
2019-11-20 17:27:19 -0800

    slider not working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,3 +2,22 @@ $('#reroll').on('click', ()=>{
   location.reload()
 });
 
+$(document).ready( ()=>{
+  let outputSpan = $('#spanOutput');
+  let sliderElement = $('#slider')
+  sliderElement.slider({
+    range : true,
+    min: 1,
+    max: 10,
+    values:[7,9],
+    slide: function(event, ui){
+      outputSpan.html(ui.values[0] + ' - ' + ui.values[1])
+      $('#txtMinScore').val(ui.values[0]);
+      $('#txtMaxScore').val(ui.values[1]);
+    }
+
+  })
+  outputSpan.html(sliderElement.slider('values', 0) + ' - ' + sliderElement.slider('values', 1))
+  $('#txtMinScore').val(sliderElement.slider('values', 0));
+  $('#txtMaxScore').val(sliderElement.slider('values', 1));
+})
